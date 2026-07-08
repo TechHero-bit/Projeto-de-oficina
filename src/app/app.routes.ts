68e98183c86be9b8b8063d90b2b6be7defc6cd59
TechHero-Bit <kevinbatista2005@gmail.com>
2026-07-08 14:51:25 -0300

    Esqueci de colocar como principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { ClientesComponent } from './components/clientes/clientes.component';
 import { VeiculosComponent } from './components/veiculos/veiculos.component';
 import { OrdensServicoComponent } from './components/ordens-servico/ordens-servico.component';
+import { LoginComponent } from './components/login/login.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
   { path: 'dashboard', component: DashboardComponent },
   { path: 'clientes', component: ClientesComponent },
   { path: 'veiculos', component: VeiculosComponent },
   { path: 'ordens-servico', component: OrdensServicoComponent },
-  { path: '**', redirectTo: '/dashboard' }
+  { path: '**', redirectTo: '/login' }
 ];
